03ba8081aa1d42a9ba59c2a234f45383f01c73ed
pontiac1-1 <suppanmiklos@gmail.com>
2018-02-19 13:55:18 +0100

    third

---
+++ week-02/day-1/printthird.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] third = new int[] { 4, 5, 6, 7 };
+            Console.WriteLine(third[2]);
+            Console.ReadLine();
+        }
+    }
+}
