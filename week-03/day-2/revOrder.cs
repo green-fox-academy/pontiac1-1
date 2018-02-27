721246c7081940d1d495b4e7a4b7e48c61b9e656
pontiac1-1 <suppanmiklos@gmail.com>
2018-02-27 16:30:13 +0100

    core(revOrder)

---
+++ week-03/day-2/revOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\users\test\source\repos\ConsoleApp74\ConsoleApp74\order.txt";
+            string[] lines = File.ReadAllLines(path);
+            for (int i = lines.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(lines[i]);
+            }
+            Console.Read();
+        }
+    }
+}
