baf4ad881e07bc03d1095ee5e848f8454521ec52
suppanmiklos@gmail.com <suppanmiklos@gmail.com>
2018-03-09 00:03:28 +0100

    core test.cs

---
+++ week-04/test.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+
+namespace TestInterface
+
+static Interface()
+{
+	protected double Add(int x, int y);
+	protected double Sub(int x, int y);
+	protected double Multi(int x, int y);
+	protected double Div(int z, int y);
+	protected double Power(int bas, int exp);
+
+}
+
