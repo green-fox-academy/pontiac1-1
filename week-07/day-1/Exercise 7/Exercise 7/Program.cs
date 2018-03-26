17a175633733c5ebde21450b7826c88c4680b32a
pontiac1-1 <suppanmiklos@gmail.com>
2018-03-26 13:32:56 +0200

    core(ex7)

---
+++ week-07/day-1/Exercise 7/Exercise 7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            //Query Syntax
+            var CityWithStartAEndI = from c in cities
+                                     where c.StartsWith('A') && c.EndsWith('I')
+                                     select c;
+
+            foreach (var city in CityWithStartAEndI)
+                Console.WriteLine(city);
+
+            Console.WriteLine();
+
+            //Method Syntax
+            var CityWithStartAEndIWithMethod = cities.Where(c => c.StartsWith('A') && c.EndsWith('I'));
+
+            foreach (var city in CityWithStartAEndIWithMethod)
+                Console.WriteLine(city);
+
+            Console.Read();
+                                     
+        }
+    }
+}
