f6babe8fb3de8783172c35d7dddcd0fb23e5e06d
pontiac1-1 <suppanmiklos@gmail.com>
2018-03-26 13:03:34 +0200

    core(ex 5)

---
+++ week-07/day-1/Exercise 5/Exercise 5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+            //Query Syntax
+            var FreqWithQuery = from n in numbers
+                                group n by n into Freq
+                                select Freq.Count();
+
+            foreach (var freq in FreqWithQuery)
+            {
+                Console.WriteLine(freq);
+            }
+
+            Console.WriteLine();
+
+            //Method Syntax
+            var FreqWithMethod = numbers.GroupBy(n => n);
+
+            foreach (var item in FreqWithMethod)
+            {
+                Console.WriteLine(item.Count());
+            }
+            
+
+            Console.Read();
+        }
+    }
+}
