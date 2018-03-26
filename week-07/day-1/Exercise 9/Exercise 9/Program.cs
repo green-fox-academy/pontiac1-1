a235e570595e906eaf0d882c2776ea7b5b6f3a7b
pontiac1-1 <suppanmiklos@gmail.com>
2018-03-26 14:36:35 +0200

    core(ex 9)

---
+++ week-07/day-1/Exercise 9/Exercise 9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] characters = new char[] { 's', 'p', 'l', 'a', 's', 'h' };
+
+            //Query Syntax
+            var charactersToStringWithQuery = String.Join("", from c in characters
+                                                              select c);
+
+            Console.WriteLine(charactersToStringWithQuery);
+
+            //Method Syntax
+            var charactersToStringWithMethod = String.Join("", characters.Select(c => c));
+
+            Console.WriteLine(charactersToStringWithMethod);
+            Console.Read();
+                                         
+
+
+        }
+    }
+}
