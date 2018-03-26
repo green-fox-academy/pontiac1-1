8a1ea590d808ca40fc89b0878b74b1bf935eabb9
pontiac1-1 <suppanmiklos@gmail.com>
2018-03-26 15:05:38 +0200

    core(ex 10)

---
+++ week-07/day-1/Exercise Ten/Exercise Ten/Fox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_Ten
+{
+    public class Fox
+    {
+        public string name;
+        public string type;
+        public string color;
+
+        public Fox(string name, string type, string color)
+        {
+            this.name = name;
+            this.type = type;
+            this.color = color;
+        }
+    }
+}
---
+++ week-07/day-1/Exercise Ten/Exercise Ten/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_Ten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var FoxList = new List<Fox>();
+            FoxList.Add(new Fox("Heki", "pallida", "green"));
+            FoxList.Add(new Fox("Kaki", "stromtrooperfox", "white"));
+            FoxList.Add(new Fox("Muki", "snowtrooperfox", "white"));
+            FoxList.Add(new Fox("Miki", "ultrafox", "orange"));
+            FoxList.Add(new Fox("Feri", "pallida", "black"));
+
+            //Query Syntax for greenfox
+            var GreenFoxesWithQuery = from fox in FoxList
+                                      where fox.color == "green"
+                                      select fox;
+
+            foreach (var fox in GreenFoxesWithQuery)
+                Console.WriteLine(fox.name);
+
+            //Method Syntax for greenfox
+            var GreenFoxesWithMethod = FoxList.Where(fox => fox.color == "green");
+
+            foreach (var fox in GreenFoxesWithMethod)
+                Console.WriteLine(fox.name);
+
+            //Query Syntax for pallida greenfox
+            var GreenOrPallidaFoxQuery = from fox in FoxList
+                                         where fox.color == "green" || fox.type == "pallida"
+                                         select fox;
+
+            foreach (var fox in GreenOrPallidaFoxQuery)
+                Console.WriteLine(fox.name);
+
+            //Method Syntax for pallida greenfox
+            var GreenOrPallidaFoxMethod = FoxList.Where(fox => fox.color == "green" || fox.type == "pallida");
+
+            foreach (var fox in GreenOrPallidaFoxMethod)
+                Console.WriteLine(fox.name);
+
+            Console.Read();
+        }
+    }
+}
