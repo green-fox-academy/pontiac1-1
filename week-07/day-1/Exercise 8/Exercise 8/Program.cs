6a099a153a197202ae6e0adf8abafd28b2ff55dd
pontiac1-1 <suppanmiklos@gmail.com>
2018-03-26 13:55:44 +0200

    core(ex 8)

---
+++ week-07/day-1/Exercise 8/Exercise 8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = "I woke up early this morning, The Sun came shining down";
+
+            //Query Syntax
+            var UpperCasesWithQuery = from c in content
+                                      where char.IsUpper(c)
+                                      select c;
+
+            foreach (var upperCaseCharacter in UpperCasesWithQuery)
+                Console.WriteLine(upperCaseCharacter);
+
+            Console.WriteLine();
+
+            //Method Syntax
+            var UpperCaseWithMethod = content.Where(c => char.IsUpper(c));
+
+            foreach (var upperCaseCharacter in UpperCaseWithMethod)
+                Console.WriteLine(upperCaseCharacter);
+
+            Console.Read();
+        }
+    }
+}
