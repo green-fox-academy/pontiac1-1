992128589236fed9056a28905590c4d0400ad705
pontiac1-1 <suppanmiklos@gmail.com>
2018-03-26 11:48:42 +0200

    core(Ex 4)

--- week-07/day-1/Exercise 4/Exercise 4/Program.cs
+++ week-07/day-1/Exercise 4/Exercise 4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exercise_4
 {
@@ -6,7 +7,26 @@ namespace Exercise_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new[] { 3, 9, 2, 8, 6, 5 };
+
+            //Query Syntax
+            var NumsWithBiggerSqThan20 = from n in numbers
+                                         where n * n > 20
+                                         select n;
+
+            foreach (var NumWithBiggerSqThan20 in NumsWithBiggerSqThan20)
+                Console.WriteLine(NumWithBiggerSqThan20);
+
+            Console.WriteLine();
+
+            //Method Syntax
+            var NumsWithBiggerSqThan20Method = numbers.Where(n => n * n > 20);
+
+            foreach (var NumWithBiggerSqthan20Method in NumsWithBiggerSqThan20Method)
+                Console.WriteLine(NumWithBiggerSqthan20Method);
+
+
+            Console.Read();
         }
     }
 }
